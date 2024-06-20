1b0cb458359ac545f9b0a8f6262e4f2bd4fb867b
Md Musfiqur Rahman <167806701+musfiqurrahmanjehad@users.noreply.github.com>
2024-06-21 00:25:10 +0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,7 @@
 // Function to generate preview based on selected player
 function generatePreview() {
   const hlsLinkInput = document.getElementById('hlsLink');
-  const embedContainer = document.getElementById('embedLink');
   const playerSelect = document.getElementById('playerSelect');
-  const previewContainer = document.getElementById('previewContainer');
-  const copyEmbedBtn = document.getElementById('copyEmbedBtn');
 
   // Get the selected player value
   const selectedPlayer = playerSelect.value;
@@ -13,53 +10,24 @@ function generatePreview() {
   let embedUrl = '';
   switch (selectedPlayer) {
     case 'cleper':
-      embedUrl = `https://noobromon.github.io/m3u8-to-iframe/live/player1.html?url=${encodeURIComponent(hlsLinkInput.value)}`;
+      embedUrl = `https://player.xdx.buzz/live/player1.html?url=${encodeURIComponent(hlsLinkInput.value)}`;
       break;
     case 'jw':
-      embedUrl = `https://noobromon.github.io/m3u8-to-iframe/live/player2.html?url=${encodeURIComponent(hlsLinkInput.value)}`;
+      embedUrl = `https://player.xdx.buzz/live/player2.html?url=${encodeURIComponent(hlsLinkInput.value)}`;
       break;
     case 'videojs':
-      embedUrl = `https://noobromon.github.io/m3u8-to-iframe/live/player3.html?url=${encodeURIComponent(hlsLinkInput.value)}`;
+      embedUrl = `https://player.xdx.buzz/live/player3.html?url=${encodeURIComponent(hlsLinkInput.value)}`;
       break; 
     case 'flowplayer':
-      embedUrl = `https://noobromon.github.io/m3u8-to-iframe/live/player4.html?url=${encodeURIComponent(hlsLinkInput.value)}`;
+      embedUrl = `https://player.xdx.buzz/live/player4.html?url=${encodeURIComponent(hlsLinkInput.value)}`;
       break;
     case 'plyrPlayer':
-      embedUrl = `https://noobromon.github.io/m3u8-to-iframe/live/player5.html?url=${encodeURIComponent(hlsLinkInput.value)}`;
+      embedUrl = `https://player.xdx.buzz/live/player5.html?url=${encodeURIComponent(hlsLinkInput.value)}`;
       break;
     default:
       break;
   }
 
-  // Update the embed link input and show copy button
-  embedContainer.value = embedUrl;
-  embedContainer.style.display = 'block';
-  copyEmbedBtn.style.display = 'inline-block';
-
-  // Update the preview container with the generated iframe
-  previewContainer.innerHTML = `<iframe src="${embedUrl}" width="560px" height="400px" frameborder="0" allowfullscreen></iframe>`;
-}
-
-// Function to copy embed link to clipboard
-function copyEmbedLink() {
-  const embedLinkInput = document.getElementById('embedLink');
-
-  // Select the embed link text
-  embedLinkInput.select();
-  embedLinkInput.setSelectionRange(0, 99999); // For mobile devices
-
-  // Copy the embed link text to the clipboard
-  document.execCommand('copy');
-
-  // Deselect the embed link input
-  embedLinkInput.setSelectionRange(0, 0);
-
-  // Change copy button text momentarily
-  const copyEmbedBtn = document.getElementById('copyEmbedBtn');
-  copyEmbedBtn.textContent = 'Copied!';
-  setTimeout(() => {
-    copyEmbedBtn.textContent = 'Copy Embed Link';
-  }, 1500);
 }
 
 // Function to load sample HLS link from JSON file
@@ -80,16 +48,4 @@ async function loadSimpleHLS() {
     console.error('Error fetching HLS URL:', error);
     alert('Failed to load HLS URL. Please try again later.');
   }
-}
-
-// Function to copy code snippet to clipboard
-function copyCode() {
-  const codeSnippet = document.querySelector('.code-snippet');
-  const codeText = codeSnippet.querySelector('code').innerText;
-
-  navigator.clipboard.writeText(codeText).then(function() {
-    alert('Code snippet copied to clipboard!');
-  }, function() {
-    alert('Failed to copy code snippet.');
-  });
 }
